141baf98e84f17a86db090059db32f6bef836d70
Mox <moxvankar2005@gmail.com>
2025-07-12 12:51:53 +0530

    feat: add basic folder structure

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 3000;
 
+// Custom Routes
+import authRoutes from './routes/auth.routes.js';
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.send("Server is running"));
+
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
