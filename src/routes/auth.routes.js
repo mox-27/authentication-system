35e692f2d1e80877b3e2a04a0afae50807241a76
Mox <moxvankar2005@gmail.com>
2025-07-12 17:28:02 +0530

    Fix Typo: auth.schema.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,7 +5,7 @@ const router = Router();
 // Controllers adn middlewares
 import { loginUser, registerUser, verifyUser } from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validate.js";
-import { registerUserSchema, loginUserSchema } from '../zodValidations/auth.shcema.js';
+import { registerUserSchema, loginUserSchema } from '../zodValidations/auth.schema.js';
 
 router.post('/register', validate(registerUserSchema), registerUser);
 router.post('/verify-user', verifyUser);
