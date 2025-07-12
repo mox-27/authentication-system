d220a281c05236c1a1d52bf014ec32a4e4402995
Mox <moxvankar2005@gmail.com>
2025-07-12 12:33:33 +0530

    chore: setup basic server with express and added dev & start script

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.send("Server is running"));
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
