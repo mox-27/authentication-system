7b0b0e36890d5897070ab5dc1cd179bd0577882a
Mox <moxvankar2005@gmail.com>
2025-07-12 13:36:34 +0530

    feat: installed zod and create registerUser schema

---
+++ src/zodValidations/auth.shcema.js
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+export const registerUserSchema = z.object({
+    name: z.string().min(1, "Name is required"),
+    email: z.email("Invalid email format"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    role: z.enum(["USER", "ADMIN"]).optional(),
+    profile_img: z.url("Must be a valid URL").optional(),
+});
