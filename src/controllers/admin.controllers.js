5a6b6a7432fecc9c4fe76c13a293d580ff1c08b1
Mox <moxvankar2005@gmail.com>
2025-07-13 17:13:52 +0530

    feat: admin-delete any user

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -7,6 +7,7 @@ export const getAllUsers = async (req, res) => {
         const isVerified = req.query.verified !== true;
         const allUsers = await client.user.findMany({
             where: { is_verified: isVerified }, select: {
+                id: true,
                 name: true,
                 email: true,
                 profile_img: true,
@@ -21,6 +22,47 @@ export const getAllUsers = async (req, res) => {
         });
     } catch (error) {
         console.error("Error: ", error);
+        return res.status(500).json({
+            status: 'error',
+            message: 'Internal server error. Please try again later.',
+        });
+    }
+};
+
+export const deleteAnyUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+
+        const user = await client.user.findUnique({
+            where: { id },
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                status: "error",
+                message: `User with id ${id} not found.`,
+            });
+        }
+
+        if (user.role === 'ADMIN') {
+            return res.status(403).json({
+                status: "error",
+                message: "Cannot delete a superadmin account.",
+            });
+        }
+
+        await client.user.delete({
+            where: { email: user.email },
+        });
+
+        return res.status(200).json({
+            status: "success",
+            message: `User with email ${user.email} deleted successfully.`,
+        });
+
+    } catch (error) {
+        console.error("Error deleting user:", error);
         return res.status(500).json({
             status: 'error',
             message: 'Internal server error. Please try again later.',
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,10 +3,11 @@ import { Router } from "express";
 const router = Router();
 
 // Controllers and middlewares
-import { getAllUsers } from "../controllers/admin.controllers.js";
+import { getAllUsers, deleteAnyUser } from "../controllers/admin.controllers.js";
 import { adminOnly, protect } from "../middlewares/protect.js";
 
 
 router.get('/users', protect, adminOnly, getAllUsers);
+router.delete('/users/:id', protect, adminOnly, deleteAnyUser);
 
 export default router;
